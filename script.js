628088e54692669fd6268604fee454fb29df29b2
Obinazo <obieropauldev@gmail.com>
2024-09-13 08:44:00 +0300

    i thought i had found a solution

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const keys = document.querySelectorAll('.calc-key');
 
 // Create a new Audio object for the click sound
-const clickSound = new Audio('mixkit-camera-shutter-click-1133.wav'); // Make sure the path to the file is correct
+const clickSound = new Audio('C:\Users\Administrator\Documents\GitHub\calculator\sounds\mixkit-camera-shutter-click-1133.wav'); // Make sure the path to the file is correct
 
 keys.forEach(key => {
   key.addEventListener('click', () => {
